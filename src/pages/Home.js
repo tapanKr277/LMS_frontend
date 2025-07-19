fce64a50a3b3b43aefd37e69a2f1b7b468c56825
Tapan Kumar <90493845+tapanKr277@users.noreply.github.com>
2025-07-19 23:29:02 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -29,47 +29,40 @@ export const Home = () => {
   }, [selectedCategory, data]);
 
   return (
-    <div className='flex justify-center '>
-      {
-        isLoading ? <Spinner /> :
-          data.length > 0 && user ?
-            <div className=''>
-              {
-                user &&
-                <div className=' flex justify-center'>
-                  <div className='flex gap-x-2 mt-2 '>
-                      <button className='button-7' onClick={() => handleCategoryFilter("All")}>All</button>
-                      <button className='button-7' onClick={() => handleCategoryFilter("Sci-Fi")}>Sci-Fi</button>
-                      <button className='button-7' onClick={() => handleCategoryFilter("Fiction")}>Fiction</button>
-                      <button className='button-7' onClick={() => handleCategoryFilter("Comedy")}>Comedy</button>
-                  </div>
-                </div>
-              }
-              <div className='grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 max-w-6xl p-2 mx-auto space-y-10 space-x-5 min-h-[80vh]'>
-                { 
-                  filterBooks.map((details) => {
-                    return <Books key={details.id} {...details} />
-                  })
-                }
-              </div>
+  <div className="flex justify-center">
+    {isLoading ? (
+      <Spinner />
+    ) : data?.length > 0 ? (
+      user ? (
+        <div>
+          <div className="flex justify-center">
+            <div className="flex gap-x-2 mt-2">
+              <button className="button-7" onClick={() => handleCategoryFilter("All")}>All</button>
+              <button className="button-7" onClick={() => handleCategoryFilter("Sci-Fi")}>Sci-Fi</button>
+              <button className="button-7" onClick={() => handleCategoryFilter("Fiction")}>Fiction</button>
+              <button className="button-7" onClick={() => handleCategoryFilter("Comedy")}>Comedy</button>
             </div>
-            :
-            data.length>0 ?
-            <div className='flex justify-center'>
-            <div className='grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 max-w-6xl p-2 mx-auto space-y-10 space-x-5 min-h-[80vh]'>
-            { 
-                  filterBooks.map((details) => {
-                    return <BooksList key={details.id} {...details} />
-                  })
-                }
-            </div>
-            </div>
-            :
-            <div className='flex justify-center items-center'>
-              <p>No Data Found</p>
-            </div>
-
-      }
-    </div>
-  )
+          </div>
+          <div className="grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 max-w-6xl p-2 mx-auto space-y-10 space-x-5 min-h-[80vh]">
+            {filterBooks.map((details) => (
+              <Books key={details.id} {...details} />
+            ))}
+          </div>
+        </div>
+      ) : (
+        <div className="flex justify-center">
+          <div className="grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 max-w-6xl p-2 mx-auto space-y-10 space-x-5 min-h-[80vh]">
+            {filterBooks.map((details) => (
+              <BooksList key={details.id} {...details} />
+            ))}
+          </div>
+        </div>
+      )
+    ) : (
+      <div className="flex justify-center items-center">
+        <p>No Data Found</p>
+      </div>
+    )}
+  </div>
+);
 }
